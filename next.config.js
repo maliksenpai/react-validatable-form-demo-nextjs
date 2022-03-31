e021cfdcf613b30f65fd344f44b5615b8cc8fa7b
maliksenpai <cramixzx@hotmail.com>
2022-03-31 14:15:05 +0300

    - bug fix about github pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? '/react-validatable-form-demo-nextjs' : '',
-  assetPrefix: isProd ? '/react-validatable-form-demo-nextjs' : '',
+  assetPrefix: 'https://maliksenpai.github.io/react-validatable-form-demo-nextjs/',
   images: {
     loader: "akamai",
     path: '',
